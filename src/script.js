f45df702bfa7444b6af95463ca47ed05a225f78b
5A5K1A <saskia.bouten@gmail.com>
2017-03-06 17:51:59 +0100

    make progress printable, smooth scroll & refactor details-summary to collapse (not fully supported yet)

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 jQuery(document).ready(function($) {
-    $('li.nav-item').on('click', function() {
-        if( $( window ).width() <= 752 ) {
-            $('button.navbar-toggle').trigger('click');
-        }
-    });
+	$('li.nav-item').on('click', function() {
+		if( $( window ).width() <= 752 ) {
+			$('button.navbar-toggle').trigger('click');
+		}
+	});
+
+	var $root = $('html, body');
+	$('a[href*=#]').on('click', function(event){
+		event.preventDefault();
+		$root.animate({
+			scrollTop: $(this.hash).offset().top
+		}, 750);
+	});
 });
