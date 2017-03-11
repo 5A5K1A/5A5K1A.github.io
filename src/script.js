62862a1ec6b8d6568f3f2d13d263f9f62b0c2a45
5A5K1A <saskia.bouten@gmail.com>
2017-03-11 11:44:44 +0100

    add console message, fix menu indicator on scroll & changed hamburger to simple word

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 jQuery(document).ready(function($) {
+
+	var MenuButton = $('button.navbar-toggle');
+	var $root      = $('html, body');
+
 	$('li.nav-item').on('click', function() {
-		if( $( window ).width() <= 752 ) {
-			$('button.navbar-toggle').trigger('click');
+		if( $(window).width() <= 752 ) {
+			MenuButton.trigger('click');
 		}
 	});
 
-	var $root       = $('html, body');
+	MenuButton.on('click', function(button) {
+		if( button.target.innerText == 'menu' ) { button.target.innerText = 'close menu'; }
+		else { button.target.innerText = 'menu'; }
+	});
 
 	$('a[href*=#]').on('click', function(e){
 		e.preventDefault();
+
+		$(document).off('scroll');
 
 		$('li.nav-item').removeClass('active');
 		$(this).parent('li.nav-item').addClass('active');
@@ -17,4 +26,31 @@ jQuery(document).ready(function($) {
 			scrollTop: $(this.hash).offset().top
 		}, 750);
 	});
+
+	if( $(window).width() > 752 ) {
+		$(document).on('scroll', menuScroll);
+	}
+
+	console.log('%cHej welkom!', 'font-size: 2.5em; color: #00ADB5;');
+	console.log("%cWat tof dat je ook mijn code bekijkt :)\nAls je graag de volledige repo wilt zien, check dan even mijn GitHub.\nhttps://github.com/5A5K1A\n\nPS: Als je tips of verbeteringen voor deze pagina weet, graag!\nSchiet ze maar in via https://github.com/5A5K1A/5A5K1A.github.io/issues\n#altijdleukomteleren", 'font-size: 1.1em; color: #393E46; line-height: 1.8em;');
+
 });
+
+function menuScroll(event){
+    var currPos = $(document).scrollTop();
+
+    $('li.nav-item.nav-main').each(function() {
+        var currLink = $(this).find('a');
+
+        var refElement = $(currLink.attr('href'));
+        var elemHeight = refElement.height() + refElement.next().height();
+
+        if( refElement.position().top <= currPos && refElement.position().top + elemHeight > currPos) {
+            $('li.nav-item').removeClass('active');
+            currLink.parent('li.nav-item').addClass("active");
+        }
+        else{
+            $(this).removeClass("active");
+        }
+    });
+}
